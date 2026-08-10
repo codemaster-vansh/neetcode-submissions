1f247fd6d7cd0d942748ca5b9ea0a829b08beb9f
Vansh Whig <vanshcodes124@gmail.com>
2026-08-10 14:59:20 +0530

    Add: asteroid-collision - submission-0

---
+++ Data Structures & Algorithms/asteroid-collision/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> st;
+        for (int a : asteroids) {
+            bool alive = true;
+            while (alive && a < 0 && !st.empty() && st.back() > 0) {
+                if (st.back() < -a) {
+                    st.pop_back();
+                } else if (st.back() == -a) {
+                    alive = false;
+                    st.pop_back();
+                } else {
+                    alive = false;
+                }
+            }
+            if (alive) st.push_back(a);
+        }
+        return st;
+    }
+};
